e0c07b505d3ef2c487eb52756befc51d915ab503
soheshdoshi <doshisohesh@gmail.com>
2019-04-25 15:04:11 +0530

    Complete rest

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
+from resources.Category import CategoryResource
+from resources.Comment import CommentResource
 
 api_bp=Blueprint('api',__name__)
+print(api_bp,__name__)
 api=Api(api_bp)
 
 api.add_resource(Hello,'/Hello')
+api.add_resource(CategoryResource, '/Category')
+api.add_resource(CommentResource, '/Comment')
--- resources/Category.py
+++ resources/Category.py
@@ -2,14 +2,14 @@ from flask import request
 from flask_restful import Resource
 from Model import db, Category, CategorySchema
 
-categories_schema = CategorySchema(many=True)
-category_schema = CategorySchema()
+categories_schema=CategorySchema(many=True)
+category_schema=CategorySchema()
 
 class CategoryResource(Resource):
     def get(self):
-        categories = Category.query.all()
-        categories = categories_schema.dump(categories).data
-        return {'status': 'success', 'data': categories}, 200
+        categories = Category.query.all
+        categories=categories_schema.dump(categories).data
+        return {'status':'success', 'data': categories}, 200
     def post(self):
         json_data = request.get_json(force=True)
         if not json_data:
@@ -48,3 +48,17 @@ class CategoryResource(Resource):
         result = category_schema.dump(category).data
 
         return { "status": 'success', 'data': result }, 204
+    def delete(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+               return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data, errors = category_schema.load(json_data)
+        if errors:
+            return errors, 422
+        category = Category.query.filter_by(id=data['id']).delete()
+        db.session.commit()
+
+        result = category_schema.dump(category).data
+
+        return { "status": 'success', 'data': result}, 204
